d90fbba645758bb7d8b1b64bac330f710beee79e
Cristian Bernal <cristianxsa15@gmail.com>
2020-05-06 22:49:24 +0200

    Improved sending emails 2

--- utils/email.js
+++ utils/email.js
@@ -12,22 +12,14 @@ module.exports = class Email {
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      return nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD
-        }
-      });
-      /*  Sendgrid
+      // Sendgrid
       return nodemailer.createTransport({
         service: 'SendGrid',
         auth: {
           user: process.env.SENDGRID_USERNAME,
           pass: process.env.SENDGRID_PASSWORD
         }
-      }); */
+      });
     }
 
     return nodemailer.createTransport({

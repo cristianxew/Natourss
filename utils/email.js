9766c9dab099fe85d1c8adacf23671c55ab243dc
Cristian Bernal <cristianxsa15@gmail.com>
2020-05-06 22:18:02 +0200

    Improved sending emails

--- utils/email.js
+++ utils/email.js
@@ -7,19 +7,27 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Jonas Schmedtmann <${process.env.EMAIL_FROM}>`;
+    this.from = `Cristian Bernal <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      // Sendgrid
+      return nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD
+        }
+      });
+      /*  Sendgrid
       return nodemailer.createTransport({
         service: 'SendGrid',
         auth: {
           user: process.env.SENDGRID_USERNAME,
           pass: process.env.SENDGRID_PASSWORD
         }
-      });
+      }); */
     }
 
     return nodemailer.createTransport({
